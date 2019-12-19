aa5ccc8246ae1695b6e3aa5f67d123e4507b1506
Matth573 <matthieu.queffelec@gmail.com>
2019-12-19 15:33:40 +0100

    Ajout de la sauvegarde en local

--- functions.py
+++ functions.py
@@ -1,28 +1,35 @@
 import os
 import sys
 import logging
-from ftplib import FTP
 
 logger = logging.getLogger('logger')
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 fileHandler = logging.FileHandler('test.log')
 logger.addHandler(fileHandler)
 formatter = logging.Formatter('%(asctime)s  %(levelname)s: %(message)s')
 fileHandler.setFormatter(formatter)
-logger.debug('This message should go to the log file')
-logger.info('So should this')
-logger.warning('And this, too')
 
 #Récupération des variables définies dans le fichier de conf:
 args=sys.argv
 REPPATH=args[1]
 METH=args[2]
-NREP=args[3]
-VERSIONNUMBER=args[4]
+IP_URL_ADDRESS=args[3]
+LOGIN=args[4]
+PASSWORD=[5]
+NREP=args[6]
+VERSIONNUMBER=args[7]
 
 
 def getpaths(directoriesPath):
     directoryList=REPPATH.split(",")
 
-    
+from os import walk
+import shutil
+def localCopy(src,dest):
+    logger.info("Copy du dossier")
+    src='/Users/matthieu/Documents/Telecom/Semestre 7/Scripting System/test'
+    dest='/Users/matthieu/Documents/Telecom/Semestre 7/Scripting System/test6'
+    destination = shutil.copytree(src,dest)
+    print(destination)
+
 
